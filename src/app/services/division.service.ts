06411e08a22830f9554ed02b5f1137bd8339b7c5
Ade Rohan Wahyudi <aderohan53@gmail.com>
2022-01-18 15:31:35 +0700

    uas

---
+++ src/app/services/division.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Division } from '../model/division.model';
+
+const baseUrl = 'https://spa-api.aqiladigital.com/api/divisions'
+@Injectable({
+  providedIn: 'root'
+})
+export class DivisionService {
+
+  constructor(private http:HttpClient) { }
+
+  getAll():Observable<Division[]>{
+    return this.http.get<Division[]>(baseUrl);
+  }
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+  delete(id:any):Observable<any>{
+    return this.http.delete(`${baseUrl}/${id}`,id);
+}
+}
